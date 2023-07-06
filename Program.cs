74f307a439033630d17182892f66f88fe0361c58
Jenira Sekar <jenirasekar@gmail.com>
2023-07-06 16:37:12 +0700

    build: i'll figure it out later

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what's your name?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"your name is {input}");
+        }
+    }
+}
