573f1b947098487ec5ad2f1fd0cca69ce1e8b502
Jenira Sekar <jenirasekar@gmail.com>
2023-07-06 20:45:51 +0700

    tipe data numerik

--- Program.cs
+++ Program.cs
@@ -4,9 +4,61 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("what's your name?");
-            string input = Console.ReadLine();
-            Console.WriteLine($"your name is {input}");
+            // Console.WriteLine("what's your name?");
+            // string input = Console.ReadLine();
+            // Console.WriteLine($"your name is {input}");
+
+            // var
+            // string name = "Shadow"; 
+            // string breed = "Golden Retriever";
+            // int age = 5;
+            // double weight = 65.22;
+            // bool spayed = true;
+            // Console.WriteLine(name);
+            // Console.WriteLine(breed);
+            // Console.WriteLine(age);
+            // Console.WriteLine(weight);
+            // Console.WriteLine(spayed);
+
+            // convert string to int, note: readline values selalu string
+            // Console.Write("Enter your favorite number!: ");      
+            // Attempt 1: use implicit conversion
+            // int faveNumber = Console.ReadLine();
+            // Attempt 2: use explicit conversion
+            // int faveNumber = (int)Console.ReadLine();
+            // Attempt 3: use Convert method
+            // int faveNumber = Convert.ToInt32(Console.ReadLine());
+
+            // tipe data numerik
+            // Number of pizza shops
+            // int pizzaShops = 4332;
+            // // Number of employees
+            // int totalEmployees = 86928;
+            // // Revenue
+            // double revenue = 390819.28;
+            // // Log the values to the console:
+            // Console.WriteLine(pizzaShops);
+            // Console.WriteLine(totalEmployees);
+            // Console.WriteLine(revenue);
+
+            // Your Age
+            // int userAge = 17;
+            // // Length of years on Jupiter (in Earth years -> revolusi ke matahari)
+            // double jupiterYears = 11.86;
+            // // Age on Jupiter
+            // double jupiterAge = userAge/jupiterYears;
+            // // Time to Jupiter
+            // double journeyToJupiter = 6.142466;
+            // // New Age on Earth
+            // double newEarthAge = userAge + journeyToJupiter;
+            // // New Age on Jupiter
+            // double newJupiterAge = newEarthAge/jupiterYears;
+            // // Log calculations to console
+            // Console.WriteLine(jupiterAge);
+            // Console.WriteLine(newEarthAge);
+            // Console.WriteLine(newJupiterAge);
+
+
         }
     }
 }
