98413c5452e546a2799956bd2af7f0debaa9c801
Jenira Sekar <jenirasekar@gmail.com>
2023-07-23 21:11:28 +0700

    membuat constructor dan void method

--- Sedan.cs
+++ Sedan.cs
@@ -1,12 +1,27 @@
 namespace interfaces
 {
     class Sedan : IAutomobile
-  {
-    public string LicensePlate { get; }
-    public double Speed { get; }
-    public int Wheels { get; }
-    public void Honk() {
-      Console.WriteLine("HONK!");
+    {
+        public string LicensePlate { get; }
+        public double Speed { get; private set; }
+        public int Wheels { get; }
+        public Sedan(double speed)
+        {
+            Speed = speed;
+            LicensePlate = Tools.GenerateLicensePlate();
+            Wheels = 4;
+        }
+        public void SpeedUp()
+        {
+            Speed += 5;
+        }
+        public void SlowDown()
+        {
+            Speed -= 5;
+        }
+        public void Honk()
+        {
+            Console.WriteLine("HONK!");
+        }
     }
-  }
 }
