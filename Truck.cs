9480cd0795ad14630c2a2eaafcd2f2461ea16c61
Jenira Sekar <jenirasekar@gmail.com>
2023-07-28 08:18:23 +0700

    implement interface 1 more time

---
+++ Truck.cs
@@ -0,0 +1,48 @@
+namespace interfaces
+{
+    class Truck : IAutomobile
+    {
+        public string LicensePlate
+        { get; }
+
+        public double Speed
+        { get; }
+
+        public int Wheels
+        { get; }
+
+        public double Weight
+        { get; }
+
+        public Truck(double speed, double weight)
+        {
+            Speed = speed;
+            LicensePlate = Tools.GenerateLicensePlate();
+            Weight = weight;
+
+            if (weight < 400)
+            {
+                Wheels = 8;
+            }
+            else
+            {
+                Wheels = 12;
+            }
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("HONK!");
+        }
+
+        public void SpeedUp()
+        {
+            Speed += 5;
+        }
+
+        public void SlowDown()
+        {
+            Speed -= 5;
+        }
+    }
+}
